2adc23f80d89f6836dd1d9a47e98634864a7c88b
mill413 <mill413@outlook.com>
2024-01-07 09:48:19 +0800

    update mdwriter

--- mdElement/__init__.py
+++ mdElement/__init__.py
@@ -4,10 +4,10 @@ from .block.heading import Heading
 from .block.horizontalrule import HorizontalRule
 from .block.image import Image
 from .block.link import Link
-from .block.list import List
+from .block.list import List, OrderedList, UnorderedList
 from .block.paragraph import Paragraph
 from .block.table import Alignment, Table
-from .element import BlockElement, InlineElement, MdElement
+from .element import BlockElement, InlineElement, MdElement, Text
 from .inline.code import Code
 from .inline.emphasis import Bold, BoldItalic, Italic
 from .inline.strikethrough import Strikethrough
--- mdWriter.py
+++ mdWriter.py
@@ -1,19 +1,21 @@
-from mdElement import MdElement, InlineElement, Paragraph
+from mdElement import MdElement, InlineElement, Paragraph, Text
 from pathlib import Path
 
 
 class MdWriter:
     def __init__(self,
-                 *content: tuple[MdElement]) -> None:
-        self.content: list[MdElement] = [Paragraph(c) if isinstance(
-            c, InlineElement) else c for c in content]
+                 *content: tuple[MdElement | str]) -> None:
+        self.content: list[MdElement | str] = [
+            Paragraph(c) if isinstance(c, InlineElement)
+            else c for c in content]
 
     def write_to_file(self, file: str | Path, mode="w"):
         md_file = file if isinstance(file, Path) else Path(file)
 
         assert md_file.exists(), f"{file} doesn't exist!"
 
-        content_str: str = "\n".join([block.md_str()+"\n"
+        content_str: str = "\n".join([f"{block.md_str()}\n" if isinstance(block, MdElement)
+                                      else block+"\n"
                                      for block in self.content])
 
         with open(md_file, mode) as f:
