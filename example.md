c952e3c2acdb5938f60c9d59b2a1717f86de0677
mill413 <mill413@outlook.com>
2024-01-12 11:07:07 +0800

    add example

---
+++ example.md
@@ -0,0 +1,69 @@
+Example Simple Text
+
+**Example Bold**
+
+*Example Italic*
+
+***Example Bold and Italic***
+
+`Example Code`
+
+~~Example Strikethrough~~
+
+# Example Heading 1
+
+## ***Example Heading 2 with Bold and Italic***
+
+> Example Blockquote
+>
+> > **Example Nested Blockquote with Bold**
+>
+> # Example Nested Heading in Blockquote
+
+Example Paragraph *Example Pagraph with Italic* Below is Example HorizontalRule
+
+---
+
+```python
+print("Example FencedCodeBlock")
+```
+
+| Header1 | **Header2 with Bold** | Header3 |
+| :--- | ---: | :---: |
+| Left | Right | Center |
+| **Bold** | *Italic* | ***Bold and Italic*** |
+| `Code` | [Link](mill413.github.io) | &#124; |
+
+[Example Link](mill413.github.io "Link Title")
+
+[**Example Link with Bold**](mill413.github.io "Link Title with Bold")
+
+[![Example Image Link](https://api.kdcc.cn "Image Link Title")](mill413.github.io "Link Title")
+
+![Example Image](https://api.kdcc.cn "Image Title")
+
+1. Example Ordered List
+2. Example Elements in Ordered List
+
+    Paragraph
+
+    > Blockquote
+
+    ![Image](https://api.kdcc.cn "Image Title")
+
+    2. Nested Ordered List starting with 2
+
+    * Nested Unordered List
+
+* Example Unordered List
+* Example Elements in Unordered List
+
+    Paragraph
+
+    > Blockquote
+
+    ![Image](https://api.kdcc.cn "Image Title")
+
+    2. Nested Ordered List starting with 2
+
+  * Nested Unordered List
---
+++ example.py
@@ -0,0 +1,67 @@
+from mdElement import (Bold, Italic, BoldItalic, Code, Strikethrough,
+                       Heading, Blockquote, Paragraph, HorizontalRule,
+                       FencedCodeBlock, Table, Alignment, Link, Image,
+                       OrderedList, UnorderedList)
+from mdWriter import MdWriter
+
+
+examples = (
+    "Example Simple Text",
+    Bold("Example Bold"),
+    Italic("Example Italic"),
+    BoldItalic("Example Bold and Italic"),
+    Code("Example Code"),
+    Strikethrough("Example Strikethrough"),
+    Heading(1, "Example Heading 1"), Heading(
+        2, BoldItalic("Example Heading 2 with Bold and Italic")),
+    Blockquote("Example Blockquote",
+               Blockquote(Bold("Example Nested Blockquote with Bold"),
+                          Heading(1, "Example Nested Heading in Blockquote"))),
+    Paragraph("Example Paragraph ", Italic(
+        "Example Pagraph with Italic"), " Below is Example HorizontalRule"),
+    HorizontalRule(),
+    FencedCodeBlock("print(\"Example FencedCodeBlock\")", "python"),
+    Table(header=["Header1", Bold("Header2 with Bold"), "Header3"],
+          content=[
+              ["Left", "Right", "Center"],
+              [Bold("Bold"), Italic("Italic"), BoldItalic("Bold and Italic")],
+              [Code("Code"), Link(url="mill413.github.io",
+                                  text_or_image="Link"), "|"]
+    ],
+        alignment=[Alignment.LEFT, Alignment.RIGHT, Alignment.CENTER]),
+    Link(url="mill413.github.io", text_or_image="Example Link", title="Link Title"),
+    Link(url="mill413.github.io", text_or_image=Bold(
+        "Example Link with Bold"), title="Link Title with Bold"),
+    Link(url="mill413.github.io", text_or_image=Image(
+        path_or_url="https://api.kdcc.cn", alt_text="Example Image Link", title="Image Link Title"), title="Link Title"),
+    Image(
+        path_or_url="https://api.kdcc.cn", alt_text="Example Image", title="Image Title"),
+    OrderedList(
+        "Example Ordered List",
+        ["Example Elements in Ordered List",
+         Paragraph("Paragraph"),
+         Blockquote("Blockquote"),
+         Image(
+             path_or_url="https://api.kdcc.cn", alt_text="Image", title="Image Title"),
+         OrderedList("Nested Ordered List starting with 2", start_index=2),
+         UnorderedList("Nested Unordered List")
+        ]
+    ),
+    UnorderedList(
+        "Example Unordered List",
+        ["Example Elements in Unordered List",
+         Paragraph("Paragraph"),
+         Blockquote("Blockquote"),
+         Image(
+             path_or_url="https://api.kdcc.cn", alt_text="Image", title="Image Title"),
+         OrderedList("Nested Ordered List starting with 2", start_index=2),
+         UnorderedList("Nested Unordered List")
+         ]
+    )
+)
+
+for e in examples:
+    print(e)
+
+mw = MdWriter(*examples)
+mw.write_to_file("./example.md")
