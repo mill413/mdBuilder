2af3c81f5bfbfb45452ea0208cf439efd0f2e664
mill413 <mill413@outlook.com>
2024-01-13 21:38:19 +0800

    fix bug in MdBuilder: it will handle str as Sequence

--- mdBuilder/MdBuilder.py
+++ mdBuilder/MdBuilder.py
@@ -9,9 +9,7 @@ class MdBuilder:
                  *content: tuple[MdElement | str | Sequence[MdElement | str]]) -> None:
         self.content: list[MdElement | str] = []
         for c in content:
-            if isinstance(c, InlineElement):
-                self.content.append(Paragraph(c))
-            elif isinstance(c, Sequence):
+            if isinstance(c, Sequence) and not isinstance(c, str):
                 for item in c:
                     self.content.append(item)
             else:
@@ -20,9 +18,9 @@ class MdBuilder:
     def write_to_file(self, file: str | Path, mode="w"):
         md_file = file if isinstance(file, Path) else Path(file)
 
-        content_str: str = "\n".join([f"{block.md_str()}\n" if isinstance(block, MdElement)
-                                      else block+"\n"
-                                     for block in self.content])
+        content_str: str = "\n".join([f"{c.md_str()}\n" if isinstance(c, MdElement)
+                                      else c+"\n"
+                                     for c in self.content])
 
         with open(md_file, mode) as f:
             f.write(content_str)
