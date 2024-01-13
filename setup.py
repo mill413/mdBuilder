7ebba46266d38eadcd38e43cf8282fca7a41d156
mill413 <mill413@outlook.com>
2024-01-13 22:16:39 +0800

    v1.0.4

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='mdBuilder',
-    version='1.0.3',
+    version='1.0.4',
     packages=setuptools.find_packages(),
     url='https://github.com/mill413/mdWriter',
     license='MIT',
