f5590a8e5cc564667704e1993f9d4e76b71d58e4
mill413 <mill413@outlook.com>
2024-01-13 21:26:28 +0800

    change Paragraph's md_str method and doc

--- mdElement/block/paragraph.py
+++ mdElement/block/paragraph.py
@@ -4,20 +4,13 @@ from ..element import BlockElement, ElementList, InlineElement
 class Paragraph(BlockElement):
     """The Paragraph Element
 
-    `Paragraph("Text1", "Text2")` corresponds to 
-
-    ```markdown
-    Text1
-
-    Text2
-    ```
-    in markdown
+    `Paragraph("Text1", Bold("Text2"))` corresponds to `Text1**Text2**` in markdown
     """
 
-    def __init__(self, *content: tuple[InlineElement, str]) -> None:
+    def __init__(self, *content: tuple[InlineElement | str]) -> None:
         super().__init__()
 
-        self.content = ElementList(content)
+        self.content = [c for c in content]
 
     def md_str(self) -> str:
-        return "".join(self.content.md_str_list()).strip()
+        return "".join(c.md_str() if isinstance(c, InlineElement) else c for c in self.content)
