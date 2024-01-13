9b86c3c658b5e745f74985e354a2e62b2585cb0a
mill413 <mill413@outlook.com>
2024-01-13 18:16:46 +0800

    add doc for table

--- mdElement/block/table.py
+++ mdElement/block/table.py
@@ -15,6 +15,16 @@ class Alignment(Enum):
 
 
 class Table(BlockElement):
+    """The Table element
+
+    Every cell's content can be str, Link, Code, Bold, Italic or BoldItalic
+
+    Attributes:
+        headers: a list of the header, which can be any type in TableElement
+        content: a list of every row's content
+        alignments: a list of every columns' alignment
+    """
+
     def __init__(self,
                  header: list[TableElement],
                  content: list[list[TableElement]],
@@ -24,20 +34,20 @@ class Table(BlockElement):
         self.contents = [ElementList(row) for row in content]
         self.alignments = alignment
 
-        self.fill_none_items()
+        self.__fill_none_items()
 
     def md_str(self) -> str:
         header_md = f"| {" | ".join(
-            self.replace_pipes(self.headers.md_str_list())
+            self.__replace_pipes(self.headers.md_str_list())
         )} |"
         content_md = "\n".join([
-            f"| {" | ".join(self.replace_pipes(row.md_str_list()))} |"
+            f"| {" | ".join(self.__replace_pipes(row.md_str_list()))} |"
             for row in self.contents])
         alignment_md = f"| {
             " | ".join([alignment.value for alignment in self.alignments])} |"
         return "\n".join((header_md, alignment_md, content_md))
 
-    def fill_none_items(self):
+    def __fill_none_items(self):
         max_columns = max((
             len(self.headers),
             *(len(content) for content in self.contents),
@@ -53,7 +63,7 @@ class Table(BlockElement):
             self.alignments.extend(
                 Alignment.LEFT*(max_columns - len(self.alignments)))
 
-    def replace_pipes(self, str_list: list[str]) -> list[str]:
+    def __replace_pipes(self, str_list: list[str]) -> list[str]:
         return [str.replace("|", "&#124;") for str in str_list]
 
     def __repr__(self) -> str:
