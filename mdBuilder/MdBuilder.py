189892829be3ae3c9b5e9e9eed73196d01a87c94
mill413 <mill413@outlook.com>
2024-01-13 20:00:47 +0800

    make MdBuilder be able to init with sequences

--- mdBuilder/MdBuilder.py
+++ mdBuilder/MdBuilder.py
@@ -1,13 +1,21 @@
-from mdElement import MdElement, InlineElement, Paragraph
 from pathlib import Path
+from typing import Sequence
+
+from mdElement import InlineElement, MdElement, Paragraph
 
 
 class MdBuilder:
     def __init__(self,
-                 *content: tuple[MdElement | str]) -> None:
-        self.content: list[MdElement | str] = [
-            Paragraph(c) if isinstance(c, InlineElement)
-            else c for c in content]
+                 *content: tuple[MdElement | str | Sequence[MdElement | str]]) -> None:
+        self.content: list[MdElement | str] = []
+        for c in content:
+            if isinstance(c, InlineElement):
+                self.content.append(Paragraph(c))
+            elif isinstance(c, Sequence):
+                for item in c:
+                    self.content.append(item)
+            else:
+                self.content.append(c)
 
     def write_to_file(self, file: str | Path, mode="w"):
         md_file = file if isinstance(file, Path) else Path(file)
