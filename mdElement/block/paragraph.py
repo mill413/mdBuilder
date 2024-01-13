30c425d68b450737d5e6ad9668787301748ec0be
mill413 <mill413@outlook.com>
2024-01-13 23:41:14 +0800

    add link support in paragraph

--- mdElement/block/paragraph.py
+++ mdElement/block/paragraph.py
@@ -1,4 +1,5 @@
-from ..element import BlockElement, ElementList, InlineElement
+from ..element import BlockElement, InlineElement
+from .link import Link
 
 
 class Paragraph(BlockElement):
@@ -7,10 +8,10 @@ class Paragraph(BlockElement):
     `Paragraph("Text1", Bold("Text2"))` corresponds to `Text1**Text2**` in markdown
     """
 
-    def __init__(self, *content: tuple[InlineElement | str]) -> None:
+    def __init__(self, *content: tuple[InlineElement | str | Link]) -> None:
         super().__init__()
 
         self.content = [c for c in content]
 
     def md_str(self) -> str:
-        return "".join(c.md_str() if isinstance(c, InlineElement) else c for c in self.content)
+        return "".join(c.md_str() if not isinstance(c, str) else c for c in self.content)
