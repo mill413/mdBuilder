0c2e678c0a3fe0946f15f91a278c9dfd3dbae0b8
mill413 <mill413@outlook.com>
2024-01-13 11:50:32 +0800

    add doc for inline elements

--- mdElement/inline/strikethrough.py
+++ mdElement/inline/strikethrough.py
@@ -2,5 +2,10 @@ from ..element import InlineElement
 
 
 class Strikethrough(InlineElement):
+    """The Strikethrough Element
+
+    Strikethrough("Text") corresponds to `~~Text~~` in markdown
+    """
+
     def __init__(self, content: str) -> None:
         super().__init__(content, symbol="~~")
